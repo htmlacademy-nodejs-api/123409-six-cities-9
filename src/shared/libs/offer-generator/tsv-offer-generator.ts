5efc27c84ec4fd64981733367be3e3fd3a11981d
Ivanov Alexander <ivanovaa1992@gmail.com>
2025-02-23 10:46:18 +0300

    feat: generate tsv

--- src/shared/libs/offer-generator/tsv-offer-generator.ts
+++ src/shared/libs/offer-generator/tsv-offer-generator.ts
@@ -6,23 +6,23 @@ export class TSVOfferGenerator implements OfferGenerator {
   constructor(private readonly mockData: MockServerData) {}
 
   public generate(): string {
-    const title = getRandomItem(this.mockData.api.titles);
-    const description = getRandomItem(this.mockData.api.descriptions);
-    const date = getRandomItem(this.mockData.api.dates);
-    const city = getRandomItem(this.mockData.api.cities);
-    const previewImage = getRandomItem(this.mockData.api.previewImages);
-    const images = getRandomItem(this.mockData.api.images).join(';');
+    const title = getRandomItem(this.mockData.titles);
+    const description = getRandomItem(this.mockData.descriptions);
+    const date = getRandomItem(this.mockData.dates);
+    const city = getRandomItem(this.mockData.cities);
+    const previewImage = getRandomItem(this.mockData.previewImages);
+    const images = getRandomItem(this.mockData.images).join(';');
     const isPremium = generateRandomValue(0, 1) === 1;
     const isFavorite = generateRandomValue(0, 1) === 1;
     const rating = generateRandomValue(1, 5);
-    const type = getRandomItem(this.mockData.api.types);
+    const type = getRandomItem(this.mockData.types);
     const bedrooms = generateRandomValue(1, 5);
     const maxAdults = generateRandomValue(1, 5);
     const price = generateRandomValue(100, 100000);
-    const comforts = getRandomItems(this.mockData.api.comforts).join(';');
-    const host = getRandomItem(this.mockData.api.users);
+    const comforts = getRandomItems(this.mockData.comforts).join(';');
+    const host = getRandomItem(this.mockData.hosts);
     const commentsCount = generateRandomValue(0, 100);
-    const coordinates = getRandomItem(this.mockData.api.coordinates);
+    const coordinates = getRandomItem(this.mockData.coordinates).join(';');
 
     return [
       title,
--- src/shared/types/mock-server.type.ts
+++ src/shared/types/mock-server.type.ts
@@ -1,18 +1,16 @@
-import { City } from './city.enum.js';
-import { Comfort } from './comfort.enum.js';
-import { HouseType } from './house-type.enum.js';
+import { City } from "./city.enum.js";
+import { Comfort } from "./comfort.enum.js";
+import { HouseType } from "./house-type.enum.js";
 
 export type MockServerData = {
-  api: {
-    titles: string[];
-    descriptions: string[];
-    cities: City[];
-    previewImages: string[];
-    images: string[][];
-    types: HouseType[];
-    comforts: Comfort[];
-    coordinates: [number, number][];
-    users: string[];
-    dates: string[];
-  };
+  titles: string[];
+  descriptions: string[];
+  cities: City[];
+  previewImages: string[];
+  images: string[][];
+  types: HouseType[];
+  comforts: Comfort[];
+  coordinates: [number, number][];
+  hosts: string[];
+  dates: string[];
 };
