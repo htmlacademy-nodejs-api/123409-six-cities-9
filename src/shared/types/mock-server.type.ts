b141497576945ded5d03e71a75b81cc8e948f3ec
Ivanov Alexander <ivanovaa1992@gmail.com>
2025-02-23 20:59:25 +0300

    eslint fix

--- src/shared/types/mock-server.type.ts
+++ src/shared/types/mock-server.type.ts
@@ -1,6 +1,6 @@
-import { City } from "./city.enum.js";
-import { Comfort } from "./comfort.enum.js";
-import { HouseType } from "./house-type.enum.js";
+import { City } from './city.enum.js';
+import { Comfort } from './comfort.enum.js';
+import { HouseType } from './house-type.enum.js';
 
 export type MockServerData = {
   titles: string[];
