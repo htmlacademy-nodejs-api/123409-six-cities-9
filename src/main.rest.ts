91b2e93607f0d3c5947a90e5383cc7148358b639
Ivanov Alexander <ivanovaa1992@gmail.com>
2025-03-11 22:19:05 +0300

    split dependency container, update user services

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -2,19 +2,13 @@ import 'reflect-metadata';
 import { Container } from 'inversify';
 
 import { Application } from './rest/application.js';
-import { Logger, PinoLogger } from './shared/libs/logger/index.js';
-import { Config, RestSchema,RestConfig } from './shared/libs/config/index.js';
 import { Component } from './shared/types/index.js';
-import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
+import { createRestApplicationContainer } from './rest/rest.container.js';
 
 function bootstrap() {
-  const container = new Container();
-  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
-  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
-  container.bind<Application>(Component.Application).to(Application).inSingletonScope();
-  container.bind<DatabaseClient>(Component.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
+  const appContainer = Container.merge(createRestApplicationContainer());
+  const application = appContainer.get<Application>(Component.Application);
 
-  const application = container.get<Application>(Component.Application);
   application.init();
 }
 
