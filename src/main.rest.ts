5bcafd50b2fab6aacde81b3bb6bf8caa557b8ca7
Ivanov Alexander <ivanovaa1992@gmail.com>
2025-03-11 22:30:53 +0300

    fix dependencies

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
 
-import { Application } from './rest/application.js';
+import { Application, createRestApplicationContainer } from './rest/index.js';
 import { Component } from './shared/types/index.js';
-import { createRestApplicationContainer } from './rest/rest.container.js';
+import { createUserContainer } from './shared/modules/user/index.js';
+
 
 function bootstrap() {
-  const appContainer = Container.merge(createRestApplicationContainer());
+  const appContainer = Container.merge(createRestApplicationContainer(), createUserContainer());
   const application = appContainer.get<Application>(Component.Application);
 
   application.init();
