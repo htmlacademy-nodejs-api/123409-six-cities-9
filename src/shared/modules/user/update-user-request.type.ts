3215ef8725d786487d400be50d118757779f115b
Ivanov Alexander <ivanovaa1992@gmail.com>
2025-04-07 22:57:45 +0300

    eslint fix

--- src/shared/modules/user/update-user-request.type.ts
+++ src/shared/modules/user/update-user-request.type.ts
@@ -3,4 +3,4 @@ import { Request } from 'express';
 import { RequestBody, RequestParams } from '../../libs/rest/index.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 
-export type UpdateUserRequest = Request<RequestParams, RequestBody, UpdateUserDto>; 
+export type UpdateUserRequest = Request<RequestParams, RequestBody, UpdateUserDto>;
