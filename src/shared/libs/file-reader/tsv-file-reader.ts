735df102ba1c7a09dabf8a495086b7479122c8f6
Ivanov Alexander <ivanovaa1992@gmail.com>
2025-04-08 21:18:51 +0300

    fix D-13

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -84,7 +84,8 @@ export class TSVFileReader extends EventEmitter implements FileReader {
     for await (const chunk of readStream) {
       remainingData += chunk.toString();
 
-      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+      nextLinePosition = remainingData.indexOf('\n');
+      while (nextLinePosition >= 0) {
         const completeRow = remainingData.slice(0, nextLinePosition + 1);
         remainingData = remainingData.slice(++nextLinePosition);
         importedRowCount++;
